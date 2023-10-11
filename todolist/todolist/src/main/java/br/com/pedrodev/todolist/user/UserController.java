83e340d06c7506b0aaa1b5768b43976db3fde3b0
Pedro26fer <p2698fernandes@gmail.com>
2023-10-11 19:28:52 -0300

    feat: Atuhorization and TaskModel

--- todolist/todolist/src/main/java/br/com/pedrodev/todolist/user/UserController.java
+++ todolist/todolist/src/main/java/br/com/pedrodev/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -19,9 +21,14 @@ public class UserController {
     public ResponseEntity create(@RequestBody UserModel userModel) {
         
         var user = this.userRepository.findByUsername(userModel.getUsername());
+
         if(user != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var hashedPassword =  BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(hashedPassword);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
